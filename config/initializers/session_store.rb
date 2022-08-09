b3f9727bb02770173bc9ee8b62626ddd5254d3d1
Jesse Shen <jly.shen@gmail.com>
2022-08-08 22:51:33 -0700

    fix error in session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,14 @@
-Rails.application.config.session_store :cookie_store, {
-  :key => 'FitPal',
-  :domain => :all,
-  :same_site => :none,
-  :secure => :true,
-  :tld_length => 2
-}
+# Rails.application.config.session_store :cookie_store
+# # , {
+# #   :key => 'FitPal',
+# #   :domain => :all,
+# #   :same_site => :none,
+# #   :secure => :true,
+# #   :tld_length => 2
+
+
+if Rails.env === 'production' 
+  Rails.application.config.session_store :cookie_store, key: 'fitPal', domain: :all, same_site: :none
+else
+  Rails.application.config.session_store :cookie_store, key: 'fitPal', same_site: :none
+end
