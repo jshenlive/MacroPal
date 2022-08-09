47751bf8afd9afa9f948180ec89119714bfcd29d
Jesse Shen <jly.shen@gmail.com>
2022-08-08 19:53:06 -0700

    fix session middleware

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,13 @@ module RailsBackEnd
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session
+# This also configures session_options for use below
+config.session_store :cookie_store, key: '_interslice_session'
+
+# Required for all session management (regardless of session_store)
+config.middleware.use ActionDispatch::Cookies
+
+config.middleware.use config.session_store, config.session_options
 
 
   end
