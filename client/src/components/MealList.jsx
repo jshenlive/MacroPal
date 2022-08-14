e0569163feac876db090ce01964e0798786d93fd
Payam Beigi <beigi.payam@gmail.com>
2022-08-14 01:11:21 +0000

    Add meal summary

--- client/src/components/MealList.jsx
+++ client/src/components/MealList.jsx
@@ -1,17 +1,40 @@
 import Axios from "axios";
 import React, { useState, useEffect, } from "react";
-import { Container, Row, Col, Form, Button, FloatingLabel } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 
 export default function MealList (props) {
 
 
   return (
-    <Container className="mt-3">
+    <Container className="mt-5">
 
-      <Row className="mb-5">
+      <Row className="mb-5 text-center">
       <Col>
-      <div>Meal list</div>
+
+      <Card className="mb-2">
+      <Card.Header>Breakfast</Card.Header>
+      <Card.Body>This is some text within a card body.</Card.Body>
+      </Card>
+
+      <Card className="mb-2">
+      <Card.Header >Lunch</Card.Header>
+      <Card.Body>This is some text within a card body.</Card.Body>
+      </Card>
+
+
+      <Card className="mb-2">
+      <Card.Header>Dinner</Card.Header>
+      <Card.Body>This is some text within a card body.</Card.Body>
+      </Card>
+
+
+      <Card className="mb-2">
+      <Card.Header>Snacks</Card.Header>
+      <Card.Body>This is some text within a card body.</Card.Body>
+      </Card>
+
+
       </Col>
 
       </Row>
