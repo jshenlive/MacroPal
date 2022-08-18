90762b5504844c8ea5bcfca5f32280920c37dfe5
Payam Beigi <beigi.payam@gmail.com>
2022-08-18 01:50:30 +0000

    Start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 import AddWorkout from './components/AddWorkout';
 import WorkoutList from './components/WorkoutList';
-import Exercise from './components/Exercise';
 import Meals from './components/Meals';
 import MealList from './components/MealList';
 import Profile from './components/Profile';
@@ -72,7 +71,6 @@ class App extends Component {
          <Route  exact path='/meals' element={<Meals state={this.state}/>}/>
          <Route  exact path='/meal-list' element={<MealList state={this.state}/>}/>
          <Route  exact path='/addworkout' element={<AddWorkout state={this.state}/>}/>
-         <Route  exact path='/exercise' element={<Exercise/>}/>
          <Route  exact path='/workoutList' element={<WorkoutList  state={this.state}/>}/>
          <Route  exact path='/summary' element={<Summary />}/>
          <Route  exact path='/profile' element={<Profile state={this.state} />}/>
